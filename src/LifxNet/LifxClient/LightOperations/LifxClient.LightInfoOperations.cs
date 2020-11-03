6b231c83d27e1a47acf19d6f41d4c52263a1ca61
DESKTOP-TVN168T\alexs <alex.stonehouse@gmail.com>
2020-11-03 16:31:44 +1100

    Added convenience method for setting colors on a light.

---
+++ src/LifxNet/LifxClient/LightOperations/LifxClient.LightInfoOperations.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace LifxNet
+{
+    public partial class LifxClient
+    {
+        public void SetColors(LightInfo light, Color[] colors, UInt16 kelvin)
+        {
+            if (light.SupportsExtendedMultiZone)
+            {
+                var spread = SpreadColors(colors, light.Zones.Length);
+                SetExtendedColorZonesAsync(light.Info, colors, kelvin, 0);
+            }
+            else if (light.SupportsMultiZone)
+            {
+                var count = light.Zones.Length;
+                SetZones(light.Info, colors, kelvin, count);
+            }
+            else
+            {
+                SetColor(light.Info, colors.First(), kelvin);
+            }
+        }
+
+        private Color[] SpreadColors(Color[] colors, int zonesCount)
+        {
+            var zonesPerColor = zonesCount / colors.Length;
+            var zoneColors = new Color[zonesCount];
+            var zonesAssignedToColor = 0;
+
+            var colorList = colors.ToList();
+            Color? color = colorList[0];
+            colorList.RemoveAt(0);
+            for (var i = 0; i < zonesCount; i++)
+            {
+                if (zonesAssignedToColor > zonesPerColor && colorList.Count() > 0)
+                {
+                    zonesAssignedToColor = 0;
+                    color = colorList[0];
+                    colorList.RemoveAt(0);
+                }
+
+                zonesAssignedToColor++;
+
+                zoneColors[i] = color.Value;
+            }
+
+            return zoneColors;
+        }
+
+        private void SetZones(LightBulb light, Color[] colors, int kelvin, int zonesCount)
+        {
+            var zonesPerColor = zonesCount / colors.Length;
+            var zoneColors = new Color[zonesCount];
+            var startIndex = 0;
+            var endIndex = zonesPerColor;
+
+            var colorList = colors.ToList();
+            Color? color = colorList[0];
+            colorList.RemoveAt(0);
+            for (var i = 0; i < colors.Length; i++)
+            {
+                SetColorZones(light, Convert.ToByte(startIndex), Convert.ToByte(endIndex), colors[i], Convert.ToByte(kelvin), 0, ZoneApplicationRequest.Apply);
+
+                startIndex = endIndex + 1;
+                endIndex += zonesPerColor;
+                if (endIndex >= zonesCount)
+                {
+                    endIndex = zonesCount - 1;
+                }
+                else if (endIndex + zonesCount > (zonesCount - 1))
+                {
+                    endIndex = zonesCount - 1;
+                }
+            }
+        }
+    }
+}
